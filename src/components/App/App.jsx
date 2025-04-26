7948dacd59c52e7a8da0e025d7e177a46f18759d
Jeronimo Corigliano <jerofc143@gmail.com>
2025-04-25 23:20:06 -0300

    Adding Loading

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,30 @@ import MyStack from '../MyStack/MyStack'
 import MyProjects from '../MyProjects/MyProjects'
 import MyStudies from '../MyStudies/MyStudies'
 import Curriculum from '../Curriculum/Curriculum'
+import { useState } from 'react'
 const App = () =>{
+
+    const [loading, setLoading] = useState(true)
+    
+
+    setTimeout(() => {
+        setLoading(false)
+        console.log("Loading finished")
+    }, 2000)
+
+    if(loading){
+        return(
+            <div className='flex flex-col justify-center items-center h-screen bg-[#111]'>
+                <svg className="animate-spin h-20 w-20 text-[#00ffaa]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 1 1 16 0A8 8 0 0 1 4 12zm2.5-1h11a2.5 2.5 0 1 1-5 0h-6a2.5 2.5 0 0 1-5 0z"></path>
+                </svg>
+                <span>
+                    <p className='text-white text-2xl font-bold'>Cargando</p>
+                </span>
+            </div>
+        )
+    }
     return( 
         <main> 
             <Header></Header>
@@ -16,7 +39,7 @@ const App = () =>{
             <MyStudies></MyStudies>
             <Curriculum></Curriculum>
         </main>
-    )
+    ) 
 }
 
 export default App
